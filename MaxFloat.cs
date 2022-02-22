db082221f34680f7455551a76968425da08e2eac
Pranali <pranalilambat00@gmail.com>
2022-02-22 22:07:24 +0530

    UC-2

---
+++ MaxFloat.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day13_Generics
+{
+    class MaxFloat
+    {
+        public static double FindMaxValue1(double Firstvalue, double Secondvalue, double Thirdvalue)
+        {
+            if (Firstvalue.CompareTo(Secondvalue) > 0 && Firstvalue.CompareTo(Thirdvalue) > 0)
+            {
+                return Firstvalue;
+            }
+            else if (Secondvalue.CompareTo(Thirdvalue) > 0 && Secondvalue.CompareTo(Thirdvalue) > 0)
+            {
+                return Secondvalue;
+            }
+            else
+            {
+                return Thirdvalue;
+            }
+
+        }
+    }
+}
